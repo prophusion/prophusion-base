496639d891426bf34259cce2253885535514836a
Mike Baynton <mike@mbaynton.com>
2017-04-24 14:49:43 -0500

    Suppress echoing xdebug config if stdout is not a tty

--- xdebug2host.sh
+++ xdebug2host.sh
@@ -2,7 +2,13 @@
 
 HOST_IP=$(/sbin/ip -4 route list 0/0 | /usr/bin/awk '/default/ { print $3 }')
 XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_host=$HOST_IP remote_port=9000"
-echo $XDEBUG_CONFIG
+
+# Print the xdebug config, but only if stdout is a tty
+if [ -t 1 ]
+then
+	echo $XDEBUG_CONFIG
+fi
+
 export XDEBUG_CONFIG
 
 if [ "$XDEBUG2HOST" != "" ]
